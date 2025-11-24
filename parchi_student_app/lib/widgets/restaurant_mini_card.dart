8b9901c28e1e58c5943d94eb3b440a5188a41de4
Umer <umernoor6660@gmail.com>
2025-11-24 18:09:56 +0500

    refactor: Making code more managable

---
+++ parchi_student_app/lib/widgets/restaurant_mini_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RestaurantMiniCard extends StatelessWidget {
+  const RestaurantMiniCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(12),
+              image: const DecorationImage(
+                image: NetworkImage("https://placehold.co/100x100/png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 4),
+        const Text(
+          "KFC",
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+        ),
+        const Text(
+          "20% OFF",
+          style: TextStyle(fontSize: 10, color: Color(0xFFE91E63), fontWeight: FontWeight.w700),
+        ),
+      ],
+    );
+  }
+}

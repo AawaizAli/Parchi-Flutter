4df13fc7f9022ead71696c6ef74207313bf32415
Aawaiz <aawaizali8@gmail.com>
2026-01-25 19:18:59 +0500

    login screen height

--- parchi_student_app/lib/screens/auth/login_screens/login_screen.dart
+++ parchi_student_app/lib/screens/auth/login_screens/login_screen.dart
@@ -78,7 +78,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen>
     if (_currentPage == 2) {
       containerHeight = screenHeight * 0.75;
     } else {
-      containerHeight = screenHeight * 0.44;
+      containerHeight = screenHeight * 0.50;
     }
 
     return Scaffold(
